1f9efbc95cb2b4e6f71dd3a820ec41c43fc71e8f
mannanzinat <mannzinat@gmail.com>
2019-03-22 20:01:31 +0600

    initial commit

---
+++ app/src/main/java/com/oxoo/spagreen/NoItemActivity.java
@@ -0,0 +1,34 @@
+package com.oxoo.spagreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class NoItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_item);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("No Item");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/oxoo/spagreen/SplashscreenActivity.java
@@ -0,0 +1,40 @@
+package com.oxoo.spagreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    private int SPLASH_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splashscreen);
+
+        getSupportActionBar().hide();
+
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(SPLASH_TIME);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashscreenActivity.this,MainActivity.class));
+                    finish();
+
+                }
+            }
+        };
+        timer.start();
+
+    }
+
+
+}
